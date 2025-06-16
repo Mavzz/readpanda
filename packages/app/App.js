6ca13d1bc72e7139f0d41effb4914552c62bc779
Venkataramaaditya Nimmagadda <30872381+Mavzz@users.noreply.github.com>
2025-06-16 22:50:38 +0530

    Delete packages/app directory
    separating the mobile repo

--- packages/app/App.js
+++
@@ -1,8 +0,0 @@
-import AppNavigator from "./src/navigation/AppNavigator";
-import "expo-dev-client";
-
-export default function App() {
-  return (
-    <AppNavigator />
-  );
-}
--- packages/app/README.md
+++
@@ -1,3 +0,0 @@
-This is goping to be the mobile application codebase for readpanda.
-
-Android folder can be used for generation of apk.
